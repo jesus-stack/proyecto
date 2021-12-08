cdaa6ae556c4a58c059bfef7602668dd3f7ccdac
Esteban Navarro <navarro.estn@gmail.com>
2021-12-08 16:46:14 -0600

    validar-asientos

--- client/app-project/src/app/component/check-out/check-out.component.ts
+++ client/app-project/src/app/component/check-out/check-out.component.ts
@@ -11,18 +11,18 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
 export class CheckOutComponent implements OnInit {
 
   vuelosAgregados: any[] = [];
-  preciosvuelo :any[] = [];
-  subtotal: any =0;
-  descuento: any =0;
-  total:any =0;
-  colon :any = 0;
+  preciosvuelo: any[] = [];
+  subtotal: any = 0;
+  descuento: any = 0;
+  total: any = 0;
+  colon: any = 0;
 
 
   constructor(
     private location: Location,
     private router: Router,
     private token: TokenStorageService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.vuelosAgregados = this.getVuelosAgregados();
@@ -38,26 +38,24 @@ export class CheckOutComponent implements OnInit {
     return [];
   }
 
-  agregarAsiento(id: any, num: number){
+  agregarAsiento(id: any, num: number) {
     for (let index = 0; index < this.vuelosAgregados.length; index++) {
 
       if (this.vuelosAgregados[index]._id === id) {
+
         this.vuelosAgregados[index].asiento = parseInt((document.getElementById(id) as HTMLInputElement).value);;
-       
-
-
-        this.vuelosAgregados[index].precio  =  this.preciosvuelo[index].precio * this.vuelosAgregados[index].asiento;
-        this.subtotal +=this.preciosvuelo[index].precio ;
-        this.descuento +=this.preciosvuelo[index].precio *(this.vuelosAgregados[index].ruta.porc_descuento/100);
-        this.total = this.subtotal-this.descuento;
-        this.colon = this.total* 633.21500;
+        this.vuelosAgregados[index].precio = this.preciosvuelo[index].precio * this.vuelosAgregados[index].asiento;
+        this.subtotal += this.preciosvuelo[index].precio;
+        this.descuento += this.preciosvuelo[index].precio * (this.vuelosAgregados[index].ruta.porc_descuento / 100);
+        this.total = this.subtotal - this.descuento;
+        this.colon = this.total * 633.21500;
 
       }
 
     }
   }
 
-  deleteVueloAgregado(id: any){
+  deleteVueloAgregado(id: any) {
     for (let index = 0; index < this.vuelosAgregados.length; index++) {
       if (this.vuelosAgregados[index]._id === id) {
         this.vuelosAgregados.splice(index, 1);
@@ -68,29 +66,37 @@ export class CheckOutComponent implements OnInit {
     this.vuelosAgregados = this.getVuelosAgregados();
   }
 
-  confirmarCompra(){
+  confirmarCompra() {
 
-    if(true){
-      if(this.token.getToken() !== null){
-       let  totales = {
+    let asientosVacios = false;
+
+    for (let i = 0; i < this.vuelosAgregados.length; i++) {
+      if (typeof this.vuelosAgregados[i].asiento === 'undefined') {
+        asientosVacios = true;
+        break;
+      }
+    }
+    if (!asientosVacios) {
+      if (this.token.getToken() !== null) {
+        let totales = {
           subtotal: this.subtotal,
           descuento: this.descuento,
-          total:this.total,
+          total: this.total,
           colon: this.colon
         }
         window.sessionStorage.setItem('precios', JSON.stringify(this.vuelosAgregados));
         window.sessionStorage.setItem('totales', JSON.stringify(totales));
         this.router.navigate(['payment']);
-      }else{
-        window.sessionStorage.setItem('redirigir_pago','true');
+      } else {
+        window.sessionStorage.setItem('redirigir_pago', 'true');
         this.router.navigate(['login']);
       }
-    }else{
-      alert("Elija los asientos");
+    } else {
+      alert("Se necesita escoger el numero de asientos para realizar el pago.");
     }
   }
 
-  goBack(){
+  goBack() {
     this.location.back();
   }
 
